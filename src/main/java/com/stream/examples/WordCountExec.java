ea4e994302baadf25b31019fd2fb72dcbc0266a1
Varun Bajaj <bajaj.varun@hotmail.com>
2018-09-13 12:20:56 +0530

    Word count example updated.

--- src/main/java/com/stream/examples/WordCountExec.java
+++ src/main/java/com/stream/examples/WordCountExec.java
@@ -1,9 +1,22 @@
 package com.stream.examples;
 
+import org.apache.kafka.common.KafkaException;
+
 public class WordCountExec {
     public static void main(String[] args) {
-        WordCount wc = new WordCountImpl();
+        WordCount wc = new WordCountImpl("localhost:9092", "wordcount");
+        wc.configure();
 
 
+        try {
+            wc.start();
+        }
+        catch (KafkaException e){
+            e.printStackTrace();
+            wc.shutdown();
+        }
+        finally {
+            wc.shutdown();
+        }
     }
 }
--- src/main/java/com/stream/examples/WordCountImpl.java
+++ src/main/java/com/stream/examples/WordCountImpl.java
@@ -1,12 +1,17 @@
 package com.stream.examples;
 
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.state.KeyValueStore;
+import org.omg.SendingContext.RunTime;
 
 import java.util.Arrays;
 import java.util.Properties;
@@ -15,6 +20,7 @@ public class WordCountImpl implements WordCount {
     private Properties props = new Properties();
     private String bootstrap = "localhost:9092";
     private String appId = "WordCount";
+    private KafkaStreams streams;
 
     public WordCountImpl(String bootstrap, String appId){
         this.bootstrap = bootstrap;
@@ -29,13 +35,13 @@ public class WordCountImpl implements WordCount {
         KStream<String, String> lines = source.stream("input-stream");
         lines
             .flatMapValues((key, values) -> Arrays.asList(values.toLowerCase().split("\\W+")))
-            .groupByKey()
-            .count()
+            .groupBy((key, value) -> value)
+            .count(Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as("counts-store"))
             .toStream()
-            .to("output-stream", Produced.with(Serdes.String(), Serdes.Long()));
+            .to("output-stream", Produced.with(Serdes.String(), Serdes.Long()))
         ;
 
-        final KafkaStreams streams = new KafkaStreams(topology, this.props);
+        streams = new KafkaStreams(topology, this.props);
         streams.start();
     }
 
@@ -45,11 +51,11 @@ public class WordCountImpl implements WordCount {
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrap);
 
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Long().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
     }
 
     @Override
     public void shutdown() {
-
+        Runtime.getRuntime().addShutdownHook(new Thread(this.streams::close));
     }
 }
